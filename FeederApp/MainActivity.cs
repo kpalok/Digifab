6f3204ab09122eda2b95c891ab31e3c08451d352
Kalle Palokangas <kalle.palokangas@gmail.com>
2018-04-15 21:58:16 +0300

    Add files via upload
    Update

--- FeederApp/MainActivity.cs
+++ FeederApp/MainActivity.cs
@@ -4,15 +4,49 @@ using Android.OS;
 using Android.Bluetooth;
 using System;
 using Android.Content;
-
+using Android.Runtime;
+using Java.Util;
 
 namespace FeederApp
 {   
+
 
     [Activity(Label = "FeederApp", MainLauncher = true)]
     public class MainActivity : Activity
     {
         static int REQUEST_ENABLE_BT = 1;
+
+        class MyReceiver : BroadcastReceiver
+        {
+            public override void OnReceive(Context context, Intent intent)
+            {
+
+                Toast.MakeText(context, "Received bluetooth device!", ToastLength.Short).Show();
+                BluetoothDevice device = (BluetoothDevice)intent.GetParcelableExtra(BluetoothDevice.ExtraDevice);
+
+                Toast.MakeText(context, device.Name, ToastLength.Short).Show();
+                BluetoothAdapter.DefaultAdapter.CancelDiscovery();
+            }    
+        }
+
+        BroadcastReceiver BtReceiver = new MyReceiver();
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            TextView txtBtLog = FindViewById<TextView>(Resource.Id.txtBtLog);
+
+            if (requestCode == REQUEST_ENABLE_BT)
+            {
+                if (resultCode == Result.Ok)
+                {
+                    txtBtLog.Text = "Bluetooth enabled.";
+                }
+            }
+        }
+
+    
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -23,7 +57,7 @@ namespace FeederApp
             SetContentView(Resource.Layout.Main);
             TextView txtFeedLog = FindViewById<TextView>(Resource.Id.txtFeedLog);
             TextView txtBtLog = FindViewById<TextView>(Resource.Id.txtBtLog);
-
+           
             BluetoothAdapter adapter = BluetoothAdapter.DefaultAdapter;
             if (adapter == null)
             {
@@ -36,17 +70,35 @@ namespace FeederApp
                 StartActivityForResult(enableBtIntetent, REQUEST_ENABLE_BT);
             }
 
+            else if (adapter.IsEnabled)
+            {
+                adapter.StartDiscovery();
+            
+                // Register for broadcasts when a device is discovered.
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ActionFound);
+                RegisterReceiver(BtReceiver, filter);
+
+                
+            }
+
+
+
             FindViewById<Button>(Resource.Id.btnMain).Click += (o, e) =>
             txtFeedLog.Text = "Last fed: " + DateTime.Now.ToString("dd/MM hh:mm");
 
-            // Register for broadcasts when a device is discovered.
-            IntentFilter filter = new IntentFilter(BluetoothDevice.ActionFound);
+            
         }
 
 
         
+    protected void onDestroy()
+        {
+            base.OnDestroy();
 
+            UnregisterReceiver(BtReceiver);
+        }
         
     }
+
 }
 
