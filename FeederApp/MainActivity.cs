9e62501f141a5203f630e156434399a4f4c2316f
Kalle Palokangas <kalle.palokangas@gmail.com>
2018-04-18 19:10:00 +0300

    Add files via upload

--- FeederApp/MainActivity.cs
+++ FeederApp/MainActivity.cs
@@ -7,6 +7,7 @@ using Android.Content;
 using Android.Runtime;
 using Java.Util;
 using System.Threading;
+using Java.IO;
 
 namespace FeederApp
 {   
@@ -15,8 +16,11 @@ namespace FeederApp
     [Activity(Label = "FeederApp", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        public Context context = null;
+        public BluetoothSocket socket = null;
         static int REQUEST_ENABLE_BT = 1;
-        byte[] WriteStream = new byte[] { 0b0 , byte.MaxValue  };
+        byte[] FEED = new byte[] { byte.MaxValue  };
+        byte[] CLEAR = new byte[] { 0b0 };
 
         BroadcastReceiver BtReceiver = new BluetoothActivity.MyReceiver();
 
@@ -24,13 +28,10 @@ namespace FeederApp
         {
             base.OnCreate(savedInstanceState);
 
-
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
             TextView txtFeedLog = FindViewById<TextView>(Resource.Id.txtFeedLog);
             TextView txtBtLog = FindViewById<TextView>(Resource.Id.txtBtLog);
-            Button ButtonMain = FindViewById<Button>(Resource.Id.btnMain);
-
+            Button button = FindViewById<Button>(Resource.Id.btnMain);
             BluetoothAdapter adapter = BluetoothAdapter.DefaultAdapter;
 
             if (adapter == null)
@@ -40,10 +41,10 @@ namespace FeederApp
             }
             else if (!adapter.IsEnabled)
             {
+                txtBtLog.Text = "Bluetooth disabled";
                 Intent enableBtIntetent = new Intent(BluetoothAdapter.ActionRequestEnable);
                 StartActivityForResult(enableBtIntetent, REQUEST_ENABLE_BT);
             }
-
             else
             {
                 adapter.StartDiscovery();
@@ -51,13 +52,99 @@ namespace FeederApp
                 // Register for broadcasts when a device is discovered.
                 IntentFilter filter = new IntentFilter(BluetoothDevice.ActionFound);
                 RegisterReceiver(BtReceiver, filter);
+                BluetoothActivity.AcceptThread accept = new BluetoothActivity.AcceptThread();
 
-                ButtonMain.Click += (o, e) =>
-                txtFeedLog.Text = "Last fed: " + DateTime.Now.ToString("dd/MM hh:mm");
+                while (true)
+                {
+                    try
+                    {
+                        socket = accept.mmServerSocket.Accept();
+                    }
+                    catch (IOException)
+                    {
+                        Toast.MakeText(context, "Socket's accept() method failed", ToastLength.Long).Show();
+                        break;
+                    }
+                    if (socket != null)
+                    {
+                        break;
+                    }
+                }
 
+                ConnectedThread connected = new ConnectedThread(socket);
+
+                button.Click += (o, e) =>
+                connected.Write(FEED);
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+                connected.Write(CLEAR);
+                txtFeedLog.Text = "Last fed: " + DateTime.Now.ToString("dd/MM hh:mm");     
             }
-        }   
-      
+        }
+
+        public class ConnectedThread
+        {
+            private BluetoothSocket mmSocket;
+            private System.IO.Stream mmInStream;
+            private System.IO.Stream mmOutStream;
+            private Context context;
+
+            public ConnectedThread(BluetoothSocket socket)
+            {
+                mmSocket = socket;
+                System.IO.Stream tmpIn = null;
+                System.IO.Stream tmpOut = null;
+
+                // Get the input and output streams; using temp objects because
+                // member streams are final.
+                try
+                {
+                    tmpIn = socket.InputStream;
+                }
+                catch (IOException)
+                {
+                    Toast.MakeText(context, "Error occured when creating input stream", ToastLength.Long).Show();
+                }
+                try
+                {
+                    tmpOut = socket.OutputStream;
+                }
+                catch (IOException)
+                {
+                    Toast.MakeText(context, "Error occured when creating output stream", ToastLength.Long).Show();
+                }
+
+                mmInStream = tmpIn;
+                mmOutStream = tmpOut;
+            }
+
+            public void Write(byte[] bytes)
+            {
+                try
+                {
+                    mmOutStream.Write(bytes, 0, 1);
+                }
+                catch (IOException)
+                {
+                    Toast.MakeText(context, "Error occured when sending data", ToastLength.Long).Show();
+                }
+            }
+
+            public void Cancel()
+            {
+                try
+                {
+                    mmSocket.Close();
+                }
+                catch (IOException)
+                {
+                    Toast.MakeText(context, "Could not close the connect socket", ToastLength.Long).Show();
+                }
+            }
+        }
+
+
+
+
         protected override void OnDestroy()
         {
             base.OnDestroy();
