96df5cfdb4ed18547ece50fad0a034c211f9b611
kpalok <kalle.palokangas@gmail.com>
2018-04-24 18:49:18 +0300

    Removed bluetooth connection within the app to maybe get it working

--- FeederApp/BluetoothFeedService.cs
+++ FeederApp/BluetoothFeedService.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Runtime.CompilerServices;
 using Android.Bluetooth;
 using Android.OS;
@@ -10,29 +11,16 @@ namespace FeederApp
 {
     class BluetoothFeedService
     {
-        const string TAG = "BluetoothReedService";
-
-        static UUID MY_UUID = UUID.FromString("250fcceb-2f35-45d3-865c-b3807bd88960");
-
-        BluetoothAdapter btAdapter;
-        Handler handler;
-        AcceptThread insecureAcceptThread;
-        ConnectThread connectThread;
+        const string TAG = "BluetoothFeedService";
         ConnectedThread connectedThread;
         int state;
-        int newState;
 
         public const int STATE_NONE = 0;
-        public const int STATE_LISTEN = 1;
-        public const int STATE_CONNECTING = 2;
-        public const int STATE_CONNECTED = 3;
+        public const int STATE_CONNECTED = 1;
 
-        public BluetoothFeedService(Handler handler)
+        public BluetoothFeedService()
         {
-            btAdapter = BluetoothAdapter.DefaultAdapter;
             state = STATE_NONE;
-            newState = state;
-            this.handler = handler;
         }
 
         [MethodImpl(MethodImplOptions.Synchronized)]
@@ -41,89 +29,14 @@ namespace FeederApp
             return state;
         }
 
-        public void Start()
-        {
-            if (connectedThread != null)
-            {
-                connectedThread.Cancel();
-                connectedThread = null;
-            }
-
-            if (insecureAcceptThread == null)
-            {
-                insecureAcceptThread = new AcceptThread(this);
-                insecureAcceptThread.Start();
-            }
-        }
-
-        public void Connect(BluetoothDevice device)
-        {
-            if (state == STATE_CONNECTING)
-            {
-                if (connectedThread != null)
-                {
-                    connectThread.Cancel();
-                    connectThread = null;
-                }
-            }
-
-            if (connectedThread != null)
-            {
-                connectedThread.Cancel();
-                connectedThread = null;
-            }
-
-            connectThread = new ConnectThread(device, this);
-            connectThread.Start();
-        }
-
-        public void Connected(BluetoothSocket socket, BluetoothDevice device)
-        {
-            // Cancel the thread that completed the connection
-            if (connectThread != null)
-            {
-                connectThread.Cancel();
-                connectThread = null;
-            }
-
-            // Cancel any thread currently running a connection
-            if (connectedThread != null)
-            {
-                connectedThread.Cancel();
-                connectedThread = null;
-            }
-
-            if (insecureAcceptThread != null)
-            {
-                insecureAcceptThread.Cancel();
-                insecureAcceptThread = null;
-            }
-
-            // Start the thread to manage the connection and perform transmissions
-            connectedThread = new ConnectedThread(socket, this);
-            connectedThread.Start();
-        }
-
+        [MethodImpl(MethodImplOptions.Synchronized)]
         public void Stop()
         {
-            if (connectThread != null)
-            {
-                connectThread.Cancel();
-                connectThread = null;
-            }
-
             if (connectedThread != null)
             {
                 connectedThread.Cancel();
                 connectedThread = null;
             }
-
-            if (insecureAcceptThread != null)
-            {
-                insecureAcceptThread.Cancel();
-                insecureAcceptThread = null;
-            }
-
             state = STATE_NONE;
         }
 
@@ -144,171 +57,6 @@ namespace FeederApp
             r.Write(@out);
         }
 
-        void ConnectionFailed()
-        {
-            state = STATE_LISTEN;
-            Start();
-        }
-
-        public void ConnectionLost()
-        {
-            state = STATE_NONE;
-            this.Start();
-        }
-
-        class AcceptThread : Thread
-        {
-            BluetoothServerSocket serverSocket;
-            BluetoothFeedService service;
-
-            public AcceptThread(BluetoothFeedService service)
-            {
-                BluetoothServerSocket tmp = null;
-                this.service = service;
-
-                try
-                {
-                    tmp = service.btAdapter.ListenUsingInsecureRfcommWithServiceRecord("FeederApp", MY_UUID);
-                }
-                catch (Java.IO.IOException e)
-                {
-                    Log.Error(TAG, "listen() failed", e);
-                }
-                serverSocket = tmp;
-                service.state = STATE_LISTEN;
-            }
-
-            public override void Run()
-            {
-                BluetoothSocket socket = null;
-
-                while (service.GetState() != STATE_CONNECTED)
-                {
-                    try
-                    {
-                        socket = serverSocket.Accept();
-                    }
-                    catch (Java.IO.IOException e)
-                    {
-                        Log.Error(TAG, "accept() failed", e);
-                        break;
-                    }
-                }
-
-                if (socket != null)
-                {
-                    lock (this)
-                    {
-                        switch (service.GetState())
-                        {
-                            case STATE_LISTEN:
-                            case STATE_CONNECTING:
-                                // Situation normal. Start the connected thread.
-                                service.Connected(socket, socket.RemoteDevice);
-                                break;
-                            case STATE_NONE:
-                            case STATE_CONNECTED:
-                                try
-                                {
-                                    socket.Close();
-                                }
-                                catch (Java.IO.IOException e)
-                                {
-                                    Log.Error(TAG, "Could not close unwanted socket", e);
-                                }
-                                break;
-                        }
-                    }
-                }
-            }
-
-            public void Cancel()
-            {
-                try
-                {
-                    serverSocket.Close();
-                }
-                catch (Java.IO.IOException e)
-                {
-                    Log.Error(TAG, "close() of server failed", e);
-                }
-            }
-        }
-
-        protected class ConnectThread : Thread
-        {
-            BluetoothSocket socket;
-            BluetoothDevice device;
-            BluetoothFeedService service;
-
-            public ConnectThread(BluetoothDevice device, BluetoothFeedService service)
-            {
-                this.device = device;
-                this.service = service;
-                BluetoothSocket tmp = null;
-
-                try
-                {
-                    tmp = device.CreateInsecureRfcommSocketToServiceRecord(MY_UUID); 
-                }
-                catch (Java.IO.IOException e)
-                {
-                    Log.Error(TAG, "create() failed", e);
-                }
-                socket = tmp;
-                service.state = STATE_CONNECTING;
-            }
-
-            public override void Run()
-            {
-                // Always cancel discovery because it will slow down connection
-                service.btAdapter.CancelDiscovery();
-
-                // Make a connection to the BluetoothSocket
-                try
-                {
-                    socket.Connect();
-                }
-                catch (Java.IO.IOException e)
-                {
-                    // Close the socket
-                    try
-                    {
-                        socket.Close();
-                    }
-                    catch (Java.IO.IOException e2)
-                    {
-                        Log.Error(TAG, "unable to close()socket during connection failure.", e2);
-                    }
-
-                    // Start the service over to restart listening mode
-                    service.ConnectionFailed();
-                    return;
-                }
-
-                // Reset the ConnectThread because we're done
-                lock (this)
-                {
-                    service.connectThread = null;
-                }
-
-                // Start the connected thread
-                service.Connected(socket, device);
-            }
-
-            public void Cancel()
-            {
-                try
-                {
-                    socket.Close();
-                }
-                catch (Java.IO.IOException e)
-                {
-                    Log.Error(TAG, "close() of connect socket failed", e);
-                }
-            }
-        }
-
         class ConnectedThread : Thread
         {
             BluetoothSocket socket;
@@ -326,13 +74,21 @@ namespace FeederApp
                 // Get the BluetoothSocket input and output streams
                 try
                 {
+                    tmpIn = socket.InputStream;
+                }
+                catch (Java.IO.IOException e)
+                {
+                    Log.Error(TAG, "temp sockets not created", e);
+                }
+                try
+                {
                     tmpOut = socket.OutputStream;
                 }
                 catch (Java.IO.IOException e)
                 {
                     Log.Error(TAG, "temp sockets not created", e);
                 }
-
+                inStream = tmpIn;
                 outStream = tmpOut;
                 service.state = STATE_CONNECTED;
             }
@@ -352,7 +108,6 @@ namespace FeederApp
                     catch (Java.IO.IOException e)
                     {
                         Log.Error(TAG, "disconnected", e);
-                        service.ConnectionLost();
                         break;
                     }
                 }
--- FeederApp/MainActivity.cs
+++ FeederApp/MainActivity.cs
@@ -2,94 +2,83 @@
 using Android.Widget;
 using Android.OS;
 using Android.Bluetooth;
-using System;
 using Android.Content;
-using Android.Runtime;
-using Java.Util;
-using System.Threading;
-using Java.IO;
+using Android.Views;
+using System;
 
 namespace FeederApp
 {   
-
-
     [Activity(Label = "FeederApp", MainLauncher = true)]
     public class MainActivity : Activity
     {
-        public Context context = null;
-        public BluetoothSocket socket = null;
         static int REQUEST_ENABLE_BT = 1;
         byte[] FEED = new byte[] { byte.MaxValue  };
         byte[] CLEAR = new byte[] { 0b0 };
+        private BluetoothFeedService service = new BluetoothFeedService();
+        private BroadcastReceiver BtReceiver = new BluetoothActivity.MyReceiver();
+        BluetoothAdapter adapter = BluetoothAdapter.DefaultAdapter;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.Main);
-            TextView txtBtLog = FindViewById<TextView>(Resource.Id.txtBtLog);
-            BluetoothAdapter adapter = BluetoothAdapter.DefaultAdapter;
-
+            Button button = FindViewById<Button>(Resource.Id.btnMain);
             if (adapter == null)
             {
 
-                txtBtLog.Text = "Device doesn't support Bluetooth!";
+                SetBtText("Device doesn't support bluetooth!");
             }
             else if (!adapter.IsEnabled)
             {
                 Intent enableBtIntetent = new Intent(BluetoothAdapter.ActionRequestEnable);
                 StartActivityForResult(enableBtIntetent, REQUEST_ENABLE_BT);
             }
+            else
+                SetBtText("Bluetooth enabled.");
         }
 
-        protected override void OnStart()
+        [Java.Interop.Export("ButtonClick")]
+        public void ButtonClick(View v)
         {
-            base.OnStart();
-
-            TextView txtFeedLog = FindViewById<TextView>(Resource.Id.txtFeedLog);
-            Button button = FindViewById<Button>(Resource.Id.btnMain);
-            BluetoothAdapter adapter = BluetoothAdapter.DefaultAdapter;
-
-            // Register for broadcasts when a device is discovered.
-            IntentFilter filter = new IntentFilter(BluetoothDevice.ActionFound);
-            RegisterReceiver(BtReceiver, filter);
-
-            button.Click += (o, e) =>
-            txtFeedLog.Text = "Last fed: " + DateTime.Now.ToString("dd/MM hh:mm");
-
+            Button button = (Button)v;
+            if (adapter == null)
+            {
+                SetFeedText("Please connect to your device!");
+            }
+            else if (adapter.IsEnabled)
+            {
+                service.Write(FEED);
+                SetFeedText("Last fed: " + DateTime.Now.ToString("dd/MM hh:mm"));
+                service.Write(CLEAR);
+            }                           
         }
 
         protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
         {
             base.OnActivityResult(requestCode, resultCode, data);
 
-            TextView txtBtLog = FindViewById<TextView>(Resource.Id.txtBtLog);
-
             if (requestCode == REQUEST_ENABLE_BT)
             {
                 if (resultCode == Result.Ok)
-                    txtBtLog.Text = "Bluetooth enabled.";
+                    SetBtText("Bluetooth enabled.");
 
                 else if (resultCode == Result.Canceled)
-                    txtBtLog.Text = "Bluetooth disabled.";
+                    SetBtText("Bluetooth disabled.");
             }
         }
 
-        protected override void OnPause()
+        public void SetBtText(string txt)
         {
-            
+            TextView txtBtLog = FindViewById<TextView>(Resource.Id.txtBtLog);
+            txtBtLog.Text = txt;
         }
 
-
-        protected override void OnDestroy()
+        public void SetFeedText(string txt)
         {
-            base.OnDestroy();
-
-            // UnregisterReceiver(BtReceiver);
-
+            TextView txtFeedLog = FindViewById<TextView>(Resource.Id.txtFeedLog);
+            txtFeedLog.Text = txt;
         }
-        
     }
-
 }
 
